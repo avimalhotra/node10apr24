91519b29de0e04dde787e36c65a5dd98b9bdc973
Avinash <avimalhotra505@gmail.com>
2024-05-05 11:25:58 +0530

    FS Module

--- src/app.js
+++ src/app.js
@@ -1,12 +1,49 @@
-//import {sum} from "./module.js";
-//const sum=require('./module');
+const os=require("os");
+const fs=require("fs");
 
-//const os=require("os");                       // from node
+//console.log(os.cpus().length);
+//console.log(os.freemem()/1073741824);
+//console.log(os.totalmem()/1073741824);
+//console.log( os.arch() );
+//console.log( os.networkInterfaces() );
+//console.log(os.platform());
+//console.log(os.uptime()/(60*60*24));
+//console.log(os.userInfo());
+//console.log( 'host ', os.hostname() );
+//console.log( os.machine() );
 
-require('colors');                              // from npm
 
-//console.log( "Hello World", sum );
-//console.log(os.freemem()/(1024*1024*1024));
-console.log('Hello World'.red);
-console.log('Hello World'.green);
-console.log('Hello World'.blue);
+//console.log( fs.readFileSync("src/data.txt").toString() );
+//console.log("done");
+
+
+/* fs.readFile("src/data.txt",'utf-8',(rej,data)=>{
+   try{
+        console.log(data);
+   }
+   catch(err){ 
+        console.warn(err);
+   }
+}); */
+
+/* fs.stat("src/data.txt",(err,res)=>{
+    try{ console.log(res.size, res.isFile(), res.isDirectory() ) }
+    catch(e){ console.warn(e);}
+}); */
+
+/* fs.writeFile("src/data.txt","Hello World",{encoding:'utf-8'},(err,res)=>{
+    try{ console.log("file updated")}
+    catch(e){ console.warn(e);}
+}); */
+
+
+/* fs.appendFile("src/data.txt",`${new Date().toLocaleString()} \n`,{encoding:'utf-8'},(err,res)=>{
+    try{ console.log("file updated")}
+    catch(e){ console.warn(e);}
+}); */
+
+fs.unlink("src/data.txt",(err,res)=>{
+    if(err){ console.log(err) }
+    else{ console.log("file deleted");}
+})
+
